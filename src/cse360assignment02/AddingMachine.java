61307d938a778240b208f19ff82606029899afed
dg5921096 <yangdalin1998@gmail.com>
2020-10-05 23:35:55 -0700

    Second version of AddingMachine

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,4 +1,16 @@
 
+
+/**
+ *
+ * @author Dalin Yang
+ * This the class of AddingMachine.java
+ *The adding machine could be used as a calculator to add and subtract numbers
+ * and Recording the history of the operation
+ *It's super useful! You gonna love it!
+ *
+ *
+ * */
+
 package cse360assignment02;
 
 import java.lang.*;
@@ -7,33 +19,52 @@ public class AddingMachine {
     private int total;
     StringBuffer history;
 
-
+    /**
+     * constructor of the AddingMachine to initialize the instance total and history.
+     */
     public AddingMachine () {
         total = 0;  // not needed - included for clarity
         history = new StringBuffer("0");
     }
 
+    /**
+     * getTotal function is the getter of the total instance
+     */
     public int getTotal () {
         return this.total;
     }
 
+    /**
+     * the add function to operate the adding to value.
+     * @param value
+     */
     public void add (int value) {
 
         total = total + value;
         history.append(" + " + value);
 
     }
-
+    /**
+     * the add function to operate the subtracting to value.
+     * @param value
+     */
 
     public void subtract (int value) {
         total = total - value;
         history.append(" - " + value);
     }
 
+    /**
+     * toString is the function to return the history result in string format.
+     */
     public String toString () {
         return history.toString();
     }
 
+
+    /**
+     * clear function to clean the data in the history object and the total instance.
+     */
     public void clear() {
         history = new StringBuffer("0");
         total = 0;
--- src/cse360assignment02/Main.java
+++ src/cse360assignment02/Main.java
@@ -1,3 +1,18 @@
+
+/**
+*
+ * @author Dalin Yang
+ * This the driver class for running and testing the AddingMachine.java
+ *The adding machine could be used as a calculator to add and subtract numbers
+ * and Recording the history of the operation
+ *It's super useful! You gonna love it!
+ *
+ *
+ * */
+
+
+
+
 package cse360assignment02;
 
 import cse360assignment02.AddingMachine;
@@ -5,6 +20,9 @@ import cse360assignment02.AddingMachine;
 public class Main {
 
     public static void main(String[] args) {
+/**
+ * Test cases for the adding machine
+ */
 
         AddingMachine myCalculator = new AddingMachine();
         myCalculator.add (6);
@@ -13,7 +31,9 @@ public class Main {
         System.out.println(myCalculator.toString());
         System.out.println("Total: " + myCalculator.getTotal());
 
-
+/**
+ * Running the clear function and print out the result after it.
+ */
         myCalculator.clear();
         System.out.println("After clear: " + myCalculator.toString());
     }
