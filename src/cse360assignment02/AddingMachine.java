853eed41a478c1e2825aae55f5d4259474b0a7aa
dg5921096 <yangdalin1998@gmail.com>
2020-10-05 20:59:01 -0700

    Second version of AddingMachine

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,33 +1,40 @@
+
 package cse360assignment02;
 
-/**
- * An adding machine for calculate and show the process
- * of calculation like
- *
- */
-
+import java.lang.*;
 
 public class AddingMachine {
     private int total;
+    StringBuffer history;
 
     public AddingMachine () {
         total = 0;  // not needed - included for clarity
+        history = new StringBuffer("0");
     }
 
     public int getTotal () {
-        return 0;
+        return this.total;
     }
 
     public void add (int value) {
+
+        this.total += value;
+        history.append(" + " + value);
+
     }
 
+
     public void subtract (int value) {
+        total -= value;
+        history.append(" - " + value);
     }
 
     public String toString () {
-        return "";
+        return history.toString();
     }
 
     public void clear() {
+        history = new StringBuffer("0");
+        total = 0;
     }
 }
